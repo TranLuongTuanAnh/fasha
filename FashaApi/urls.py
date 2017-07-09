5e061c0842ebecd03fb0ea6653a3c05e7dddb842
TranLuongTuanAnh <tlta.bkhn@gmail.com>
2017-07-10 00:35:23 +0900

    added 404_error_response

--- FashaApi/urls.py
+++ FashaApi/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from . import views
+from . import views,response_error_views
 
 urlpatterns = [
  url(r'^register/$', views.UserRegister.as_view()),
  url(r'^login/$', views.login,name='login'),
  url(r'^index/$', views.index, name='index'),
+ url(r'^not_authenticated/$', response_error_views.not_authenticated, name='not_authenticated')
 ]
